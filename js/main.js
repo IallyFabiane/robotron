4db15c03037470f1a7a082fa8c3053ae20e47324
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-01-12 10:38:12 -0300

    funcionalidade do braço do robô

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 const robotron = document.querySelector("#robotron");
+const subtrair = document.getElementById("subtrair");
+const somar = document.getElementById("somar");
+const braco = document.getElementById("braco");
 
-robotron.addEventListener('click', () => console.log('oi'))
+robotron.addEventListener('click', dizOi('Ially'));
+somar.addEventListener('click', () => {
+   parseInt(braco.value++);
+})
+subtrair.addEventListener('click', () => {
+    parseInt(braco.value--);
+ })
+
+function dizOi(nome) {
+    console.log(nome + " bem-vinda ao Robotron 2000")
+}
+
